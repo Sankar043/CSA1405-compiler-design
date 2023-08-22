6dbfa13e517d003d56faa3d96f7ab367fefdfd09
Sankar043 <112852342+Sankar043@users.noreply.github.com>
2023-08-22 12:16:15 +0530

    Update 11.First.c

--- 11.First.c
+++ 11.First.c
@@ -1,47 +1,97 @@
 #include<stdio.h>
 #include<ctype.h>
-void FIRST(char);
-int count,n=0;
-char prodn[10][10],first[10];
-main()
+void FIRST(char[],char );
+void addToResultSet(char[],char);
+int numOfProductions;
+char productionSet[10][10];
+int main()
 {
-int i,choice;
-char c,ch;
-printf("How many productions:");
-scanf("%d",&count);
-printf("Enter %d productions epsilon= $ :\n\n",count);
-for(i=0;i<count;i++)
-scanf("%s%c",prodn[i],&ch);
-do
-{
-n=0;
-printf("Element :");
-scanf("%c",&c);
-FIRST(c);
-printf("\n FIRST(%c)= { ",c);
-for(i=0;i<n;i++)
-printf("%c ",first[i]);
-printf("}\n");
-printf("press 1 to continue : ");
-scanf("%d%c",&choice,&ch);
+    int i;
+    char choice;
+    char c;
+    char result[20];
+    printf("How many number of productions ? :");
+    scanf(" %d",&numOfProductions);
+    for(i=0;i<numOfProductions;i++)//read production string eg: E=E+T
+    {
+        printf("Enter productions Number %d : ",i+1);
+        scanf(" %s",productionSet[i]);
+    }
+    do
+    {
+        printf("\n Find the FIRST of  :");
+        scanf(" %c",&c);
+        FIRST(result,c); //Compute FIRST; Get Answer in 'result' array
+        printf("\n FIRST(%c)= { ",c);
+        for(i=0;result[i]!='\0';i++)
+        printf(" %c ",result[i]);       //Display result
+        printf("}\n");
+         printf("press 'y' to continue : ");
+        scanf(" %c",&choice);
+    }
+    while(choice=='y'||choice =='Y');
 }
-while(choice==1);
-}
-void FIRST(char c)
+/*
+ *Function FIRST:
+ *Compute the elements in FIRST(c) and write them
+ *in Result Array.
+ */
+void FIRST(char* Result,char c)
 {
-int j;
-if(!(isupper(c)))
-first[n++]=c;
-for(j=0;j<count;j++)
-{
-if(prodn[j][0]==c)
-{
-if(prodn[j][2]=='$')
-first[n++]='$';
-else if(islower(prodn[j][2]))
-first[n++]=prodn[j][2];
-else
-FIRST(prodn[j][2]);
+    int i,j,k;
+    char subResult[20];
+    int foundEpsilon;
+    subResult[0]='\0';
+    Result[0]='\0';
+    //If X is terminal, FIRST(X) = {X}.
+    if(!(isupper(c)))
+    {
+        addToResultSet(Result,c);
+               return ;
+    }
+    //If X is non terminal
+    //Read each production
+    for(i=0;i<numOfProductions;i++)
+    {
+//Find production with X as LHS
+        if(productionSet[i][0]==c)
+        {
+//If X ? e is a production, then add e to FIRST(X).
+ if(productionSet[i][2]=='$') addToResultSet(Result,'$');
+            //If X is a non-terminal, and X ? Y1 Y2 … Yk
+            //is a production, then add a to FIRST(X)
+            //if for some i, a is in FIRST(Yi),
+            //and e is in all of FIRST(Y1), …, FIRST(Yi-1).
+      else
+            {
+                j=2;
+                while(productionSet[i][j]!='\0')
+                {
+                foundEpsilon=0;
+                FIRST(subResult,productionSet[i][j]);
+                for(k=0;subResult[k]!='\0';k++)
+                    addToResultSet(Result,subResult[k]);
+                 for(k=0;subResult[k]!='\0';k++)
+                     if(subResult[k]=='$')
+                     {
+                         foundEpsilon=1;
+                         break;
+                     }
+                 if(!foundEpsilon)
+                     break;
+                 j++;
+                }
+            }
+    }
 }
+    return ;
 }
+void addToResultSet(char Result[],char val)
+{
+    int k;
+    for(k=0 ;Result[k]!='\0';k++)
+        if(Result[k]==val)
+            return;
+    Result[k]=val;
+    Result[k+1]='\0';
 }
